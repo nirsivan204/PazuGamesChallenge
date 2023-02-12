19c703d7d24c01eb4bc37b70366604090452d601
nir sivan <nirsivan204@gmail.com>
2023-02-12 02:13:11 +0200

    hair dryer rotation is pointing hair

--- Assets/Scripts/HairDryer.cs
+++ Assets/Scripts/HairDryer.cs
@@ -4,16 +4,57 @@ using UnityEngine;
 
 public class HairDryer : AbstractTool
 {
-    // Start is called before the first frame update
+    bool isFlipped = false;
+    bool isTaken = false;
+    float _originalRotationAngle;
+
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] HairManager HairManager;
+
     protected override void Start()
     {
         base.Start();
         _toolName = Tools.Dryer;
+        _originalRotationAngle = transform.rotation.eulerAngles.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator UpdateRotation()
     {
-        
+        while (isTaken)
+        {
+            if ((transform.position.x > 0 && !isFlipped) ||
+                (transform.position.x < 0 && isFlipped))
+            {
+                flip();
+            }
+            LookAt2D(HairManager.transform.position);
+            yield return null;
+        }
     }
+
+    void LookAt2D(Vector3 position)
+    {
+        float angle = Vector3.SignedAngle(Vector3.up, position - transform.position,Vector3.forward);
+        transform.rotation = Quaternion.Euler(0, isFlipped?180:0, angle* (isFlipped ? -1 : 1));
+    }
+
+    void flip()
+    {
+        isFlipped = !isFlipped;
+    }
+
+    public override void Take()
+    {
+        base.Take();
+        isTaken = true;
+        StartCoroutine(UpdateRotation());
+    }
+
+    public override void Return()
+    {
+        base.Return();
+        isTaken = false;
+        transform.rotation = Quaternion.Euler(0, 0, _originalRotationAngle);
+    }
+
 }
